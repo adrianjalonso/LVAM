c700106963d48a7f2397e84414866c1637667d19
Adrián Alonso <adrianjalonso@gmail.com>
2025-10-30 18:59:56 -0300

    Adicionei botão de favoritos e contagem de favoritos

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import { Route, Routes } from "react-router-dom"
 import Login from "./Login"
 import PaginaPrincipal from "./PaginaPrincipal"
-
-
+import Favoritos from "./Favoritos"
+import Header from "./Header"
+import Footer from "./Footer"
+import { useState } from "react"
 
 
 export default function App () {
+
+  const [favoritos, setFavoritos] = useState<number[]>([]);
+  const [busca, setBusca] = useState("")
+
+
+
   return (
     <div className="h-full w-full flex justify-center flex-col ">
+      <Header isFavorited={favoritos.length} 
+      onSearchChange={setBusca}
+      className={
+          "w-full left-0 top-0 fixed h-14 bg-white border-b flex justify-between items-center px-4 md:px-10 py-4 z-10"
+        } />
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/Login" element={<Login />} />
-      <Route path="/PaginaPrincipal" element={<PaginaPrincipal />} />
+      <Route path="/PaginaPrincipal" element={<PaginaPrincipal busca={busca} setBusca={setBusca} favoritos={favoritos} setFavoritos={setFavoritos}/>} />
+      <Route path="/Favoritos" element={<Favoritos />} />
     </Routes>
+    <Footer className={
+          "w-full justify-center flex left-0 bottom-0 fixed h-8 bg-primary"
+        } />
     </div>
   )
 }
---
+++ src/Favoritos.tsx
@@ -0,0 +1,21 @@
+
+
+export default function Favoritos(){
+
+  
+
+  return(
+    <div className="flex justify-center items-center h-screen gap-5">
+      
+      
+      <p>aqui van los favoritos</p>
+      <p>aqui van los favoritos</p>
+      <p>aqui van los favoritos</p>
+      <p>aqui van los favoritos</p>
+      <p>aqui van los favoritos</p>
+      <p>aqui van los favoritos</p>
+      <p>aqui van los favoritos</p>
+      <p>aqui van los favoritos</p>
+    </div>
+  )
+}
